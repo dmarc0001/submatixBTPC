d0b5c85bb0a9a2206002d66f68ca505e29585f5e
Dirk.Marciniak <dirk_marciniak@arcor.de>
2012-04-22 21:41:22 +0200

    Logpanel eingebaut

---
+++ src/de/dmarcini/submatix/pclogger/gui/spx42LoglistPanel.java
@@ -0,0 +1,82 @@
+package de.dmarcini.submatix.pclogger.gui;
+
+import java.awt.BorderLayout;
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+import java.util.logging.Logger;
+
+import javax.swing.JPanel;
+
+/**
+ * 
+ * Panel zeigt die Liste der Logeinträge an
+ * 
+ * Project: SubmatixBTForPC Package: de.dmarcini.submatix.pclogger.gui
+ * 
+ * @author Dirk Marciniak (dirk_marciniak@arcor.de)
+ * 
+ *         Stand: 22.04.2012
+ */
+public class spx42LoglistPanel extends JPanel
+{
+  /**
+   * 
+   */
+  private static final long serialVersionUID = 1L;
+  protected Logger          LOGGER           = null;
+
+  /**
+   * Create the panel.
+   */
+  @SuppressWarnings( "unused" )
+  private spx42LoglistPanel()
+  {
+    setLayout( new BorderLayout( 0, 0 ) );
+    initPanel();
+  }
+
+  public spx42LoglistPanel( Logger LOGGER )
+  {
+    this.LOGGER = LOGGER;
+    initPanel();
+  }
+
+  private void initPanel()
+  {
+    //
+  }
+
+  public void setGlobalChangeListener( MainCommGUI mainCommGUI )
+  {
+    //
+  }
+
+  public int setLanguageStrings( ResourceBundle stringsBundle )
+  {
+    try
+    {
+      //
+    }
+    catch( NullPointerException ex )
+    {
+      System.out.println( "ERROR set language strings <" + ex.getMessage() + "> ABORT!" );
+      return( -1 );
+    }
+    catch( MissingResourceException ex )
+    {
+      System.out.println( "ERROR set language strings - the given key can be found <" + ex.getMessage() + "> ABORT!" );
+      return( 0 );
+    }
+    catch( ClassCastException ex )
+    {
+      System.out.println( "ERROR set language strings <" + ex.getMessage() + "> ABORT!" );
+      return( 0 );
+    }
+    return( 1 );
+  }
+
+  public void setAllGasPanelsEnabled( boolean en )
+  {
+    //
+  }
+}

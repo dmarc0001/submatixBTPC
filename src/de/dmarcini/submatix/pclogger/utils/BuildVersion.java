5ec370353b10d67017a69dc05aa68979c5ba4475
Dirk.Marciniak <dirk_marciniak@arcor.de>
2012-08-05 16:05:25 +0200

    Automatische BuildVersion

--- src/de/dmarcini/submatix/pclogger/utils/BuildVersion.java
+++ src/de/dmarcini/submatix/pclogger/utils/BuildVersion.java
@@ -17,8 +17,8 @@ import de.dmarcini.submatix.pclogger.res.ProjectConst;
  */
 public class BuildVersion
 {
-  private final long buildNumber = 548L;
-  private final long buildDate = 1344025922112L;
+  private final long buildNumber = 549L;
+  private final long buildDate = 1344026263101L;
 
   /**
    * Gib die Buildnummer zurück

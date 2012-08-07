d12461ac32e81d64cfc05712381d65180406efbb
Dirk.Marciniak <dirk_marciniak@arcor.de>
2012-08-07 22:35:33 +0200

    Logformatter übersichtlicher gemacht, Loglevel nach dem Datum

--- src/de/dmarcini/submatix/pclogger/utils/DirksConsoleLogFormatter.java
+++ src/de/dmarcini/submatix/pclogger/utils/DirksConsoleLogFormatter.java
@@ -5,68 +5,60 @@ import java.util.Date;
 import java.util.logging.Formatter;
 import java.util.logging.LogRecord;
 
-
 public class DirksConsoleLogFormatter extends Formatter
 {
-  private final Date date = new Date();
+  private final Date       date         = new Date();
   private final DateFormat dateformater = DateFormat.getDateTimeInstance( DateFormat.SHORT, DateFormat.MEDIUM );
-  private String subst = "";
-  
-  
+  private String           subst        = "";
+
   public DirksConsoleLogFormatter( String sub )
   {
     subst = sub;
   }
-  
+
   /**
    * @see java.util.logging.Formatter#format(java.util.logging.LogRecord)
    */
   @Override
-  public String format(LogRecord record) 
+  public String format( LogRecord record )
   {
-    date.setTime(record.getMillis());
-
+    date.setTime( record.getMillis() );
     StringBuffer sb = new StringBuffer();
-
     // Datum formatieren und in den Puffer
-    sb.append( "[" + dateformater.format(date) + "]" );
-    sb.append(" ");
-
+    sb.append( "[" + dateformater.format( date ) + "]" );
+    sb.append( " " );
+    // Level
+    sb.append( record.getLevel().getName() );
+    sb.append( " " );
     // Klasse oder Logger-Name vergewaltigen
-    if (record.getSourceClassName() != null) 
+    if( record.getSourceClassName() != null )
     {
       if( subst.isEmpty() )
       {
-        sb.append( record.getSourceClassName()  );
+        sb.append( record.getSourceClassName() );
       }
       else
       {
-        sb.append( (record.getSourceClassName()).replaceFirst( subst, "" )  );
+        sb.append( ( record.getSourceClassName() ).replaceFirst( subst, "" ) );
       }
-    } 
-    else 
+    }
+    else
     {
-      sb.append(record.getLoggerName());
+      sb.append( record.getLoggerName() );
     }
-
     // Method
-    if (record.getSourceMethodName() != null) {
-      sb.append(" ");
-      sb.append(record.getSourceMethodName());
-      sb.append("() ");
+    if( record.getSourceMethodName() != null )
+    {
+      sb.append( " " );
+      sb.append( record.getSourceMethodName() );
+      sb.append( "() " );
     }
-
-    // Level
-    sb.append(record.getLevel().getName() );
-    sb.append(" - ");
-
+    // Tennstrich
+    sb.append( " - " );
     // Message
-    sb.append(record.getMessage() );
-
+    sb.append( record.getMessage() );
     // Newline
-    sb.append(System.getProperty("line.separator") );
-
+    sb.append( System.getProperty( "line.separator" ) );
     return sb.toString();
   }
-
 }

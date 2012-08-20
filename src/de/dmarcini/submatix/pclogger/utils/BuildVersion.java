d521d64f257af6904e70409d8bc2ec962dac2b7e
Dirk.Marciniak <dirk_marciniak@arcor.de>
2012-08-20 19:50:20 +0200

    Version hochgezählt

--- src/de/dmarcini/submatix/pclogger/utils/BuildVersion.java
+++ src/de/dmarcini/submatix/pclogger/utils/BuildVersion.java
@@ -17,8 +17,8 @@ import de.dmarcini.submatix.pclogger.res.ProjectConst;
  */
 public class BuildVersion
 {
-  private final long buildNumber = 711L;
-  private final long buildDate = 1345160456128L;
+  private final long buildNumber = 721L;
+  private final long buildDate = 1345484695282L;
 
   /**
    * Gib die Buildnummer zurück

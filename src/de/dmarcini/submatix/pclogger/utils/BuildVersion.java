1361dea7084b9eedb700891f208c412587b30627
Dirk Marciniak <dirk_marciniak@arcor.de>
2012-11-16 17:17:47 +0100

    Automatisierte Versionierung

--- src/de/dmarcini/submatix/pclogger/utils/BuildVersion.java
+++ src/de/dmarcini/submatix/pclogger/utils/BuildVersion.java
@@ -17,8 +17,8 @@ import de.dmarcini.submatix.pclogger.res.ProjectConst;
  */
 public class BuildVersion
 {
-  private final long buildNumber = 1653L;
-  private final long buildDate = 1351786066717L;
+  private final long buildNumber = 1655L;
+  private final long buildDate = 1353082379351L;
 
   /**
    * Gib die Buildnummer zurück

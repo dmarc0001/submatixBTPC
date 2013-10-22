e3ede329a389c49ef467f0f5d58a340a50f7a9b7
Dirk Marciniak <dirk_marciniak@arcor.de>
2013-10-22 17:18:26 +0200

    Überarbeitung der Konfig

--- src/de/dmarcini/submatix/pclogger/utils/SpxPcloggerProgramConfig.java
+++ src/de/dmarcini/submatix/pclogger/utils/SpxPcloggerProgramConfig.java
@@ -7,7 +7,6 @@
  */
 package de.dmarcini.submatix.pclogger.utils;
 
-
 import java.io.File;
 
 import org.apache.log4j.Level;
@@ -20,19 +19,18 @@ import de.dmarcini.submatix.pclogger.res.ProjectConst;
  * 
  * @author Dirk Marciniak (dirk_marciniak@arcor.de) Stand: 18.07.2012
  */
-@SuppressWarnings("javadoc")
 public class SpxPcloggerProgramConfig
 {
   // statische Variablen, sind IMMER fürs ganze Programm gleich
   public static int        unitsProperty     = ProjectConst.UNITS_DEFAULT;
-  public static final File configFile        = new File(System.getProperty("user.dir") + File.separator + ProjectConst.CONFIGFILENAME);
-  public static final File programDir        = new File(System.getProperty("user.dir"));
+  public static final File configFile        = new File( System.getProperty( "user.home" ) + File.separator + ProjectConst.CONFIGFILENAME );
+  public static final File programDir        = new File( System.getProperty( "user.dir" ) );
   public static boolean    consoleLog        = false;
   public static Logger     LOGGER            = Logger.getRootLogger();
   public static Level      logLevel          = Level.FATAL;
-  public static File       logFile           = new File(programDir.getAbsolutePath() + File.separator + ProjectConst.DEFAULTLOGFILE);
-  public static File       databaseDir       = new File(programDir.getAbsolutePath() + File.separator + ProjectConst.DEFAULTDATADIR);
-  public static File       exportDir         = new File(programDir.getAbsolutePath() + File.separator + ProjectConst.DEFAULTEXPORTDIR);
+  public static File       logFile           = new File( System.getProperty( "user.home" ) + File.separator + ProjectConst.DEFAULTLOGFILE );
+  public static File       databaseDir       = new File( System.getProperty( "user.home" ) + File.separator + ProjectConst.DEFAULTDATADIR );
+  public static File       exportDir         = new File( System.getProperty( "user.home" ) + File.separator + ProjectConst.DEFAULTEXPORTDIR );
   public static String     langCode          = null;
   public static boolean    wasCliLogfile     = false;
   public static boolean    wasCliLogLevel    = false;
